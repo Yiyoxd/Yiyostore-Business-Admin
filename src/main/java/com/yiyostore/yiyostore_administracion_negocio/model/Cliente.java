7d3ceee5700f07da4744b18b83d07175bb5b2390
Alfredo Alberto Palacios Rodriguez <yiyoaapr@gmail.com>
2024-08-12 13:24:54 -0600

    Eliminar fecha de primera compra, permitir campos nulos para nombre y teléfono, e implementar cliente por defecto
    - Eliminado el campo fechaDePrimeraCompra de la clase Cliente.
    - Permitido que los campos nombre, numeroTelefono y nombreEnRedes sean nulos (nullable).
    - Implementado un cliente por defecto con valores predefinidos para manejar ventas a desconocidos.
    - Agregadas anotaciones JPA para reflejar estos cambios en la base de datos.

--- src/main/java/com/yiyostore/yiyostore_administracion_negocio/model/Cliente.java
+++ src/main/java/com/yiyostore/yiyostore_administracion_negocio/model/Cliente.java
@@ -1,56 +1,95 @@
 package com.yiyostore.yiyostore_administracion_negocio.model;
 
-import java.time.LocalDate;
 import java.util.Objects;
 
 import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
- 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
 /**
- * Representa un cliente. Incluye todos
+ * Representa un cliente en el sistema. Esta clase incluye todos
  * los detalles necesarios para administrar la relación con el cliente, como
- * identificación, información de contacto, compras realizadas, y otros
+ * identificación, información de contacto, y otros
  * detalles relevantes.
  */
+@Entity
+@Table(name = "clientes")
 public class Cliente {
 
     private static final PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
-                            
+
     /**
      * Identificador único del cliente asignado por el sistema.
+     * Este valor es generado automáticamente por la base de datos.
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
     /**
      * Nombre real del cliente.
+     * Este campo es opcional y puede ser nulo en caso de que el cliente no proporcione un nombre.
      */
+    @Column(name = "nombre")
     private String nombre;
 
     /**
-     * Nombre en Facebook del cliente.
+     * Nombre del cliente en redes sociales.
+     * Este campo es opcional y se utiliza para almacenar el nombre del cliente en plataformas de redes sociales.
      */
-    private String nombreEnFacebook;
+    @Column(name = "nombre_en_redes")
+    private String nombreEnRedes;
 
     /**
      * Dirección residencial del cliente.
+     * Relación uno a uno con la entidad {@link Direccion}. Esta relación permite que cada cliente tenga una única dirección asociada.
+     * La dirección se carga de manera perezosa (lazy) y todas las operaciones de persistencia realizadas en el cliente
+     * se propagarán a la dirección.
      */
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Direccion direccion;
 
     /**
-     * Fecha en que el cliente hizo su primera compra.
-     */
-    private LocalDate fechaDePrimeraCompra;
-
-    /**
      * Número de teléfono del cliente para llamadas y/o WhatsApp.
+     * Este campo es opcional y puede ser nulo si el cliente no proporciona un número de teléfono.
+     * Se realiza una validación del número de teléfono antes de asignarlo, utilizando la biblioteca libphonenumber.
      */
+    @Column(name = "numero_telefono")
     private String numeroTelefono;
 
     /**
-     * Notas sobre el cliente.
+     * Notas adicionales sobre el cliente.
+     * Este campo es opcional y puede contener cualquier información adicional relevante sobre el cliente.
      */
+    @Column(name = "notas")
     private String notas;
+
+    // Constructor por defecto
+    public Cliente() {
+        // Constructor vacío necesario para JPA
+    }
+
+    // Constructor para cliente por defecto
+    public Cliente(boolean esClientePorDefecto) {
+        if (esClientePorDefecto) {
+            this.nombre = "Cliente Desconocido";
+            this.nombreEnRedes = "NA";
+            this.direccion = null;
+            this.numeroTelefono = "NA";
+            this.notas = "Cliente por defecto para ventas a desconocidos.";
+        }
+    }
 
     /**
      * Obtiene el identificador único del cliente.
@@ -73,7 +112,7 @@ public class Cliente {
     /**
      * Obtiene el nombre real del cliente.
      *
-     * @return el nombre real del cliente.
+     * @return el nombre real del cliente, o null si no tiene un nombre registrado.
      */
     public String getNombre() {
         return nombre;
@@ -89,21 +128,21 @@ public class Cliente {
     }
 
     /**
-     * Obtiene el nombre en Facebook del cliente.
+     * Obtiene el nombre del cliente en redes sociales.
      *
-     * @return el nombre en Facebook del cliente.
+     * @return el nombre en redes del cliente.
      */
-    public String getNombreEnFacebook() {
-        return nombreEnFacebook;
+    public String getNombreEnRedes() {
+        return nombreEnRedes;
     }
 
     /**
-     * Establece el nombre en Facebook del cliente.
+     * Establece el nombre del cliente en redes sociales.
      *
-     * @param nombreEnFacebook el nuevo nombre en Facebook del cliente.
+     * @param nombreEnRedes el nuevo nombre en redes del cliente.
      */
-    public void setNombreEnFacebook(String nombreEnFacebook) {
-        this.nombreEnFacebook = nombreEnFacebook;
+    public void setNombreEnRedes(String nombreEnRedes) {
+        this.nombreEnRedes = nombreEnRedes;
     }
 
     /**
@@ -125,27 +164,9 @@ public class Cliente {
     }
 
     /**
-     * Obtiene la fecha en que el cliente hizo su primera compra.
-     *
-     * @return la fecha de la primera compra del cliente.
-     */
-    public LocalDate getFechaDePrimeraCompra() {
-        return fechaDePrimeraCompra;
-    }
-
-    /**
-     * Establece la fecha en que el cliente hizo su primera compra.
-     *
-     * @param fechaDePrimeraCompra la nueva fecha de la primera compra del cliente.
-     */
-    public void setFechaDePrimeraCompra(LocalDate fechaDePrimeraCompra) {
-        this.fechaDePrimeraCompra = fechaDePrimeraCompra;
-    }
-
-    /**
      * Obtiene el número de teléfono del cliente.
      *
-     * @return el número de teléfono del cliente.
+     * @return el número de teléfono del cliente, o null si no tiene un número registrado.
      */
     public String getNumeroTelefono() {
         return numeroTelefono;
@@ -159,23 +180,23 @@ public class Cliente {
      * @throws IllegalArgumentException si el número de teléfono no es válido.
      */
     public void setNumeroTelefono(String numeroTelefono) {
-        if (!isValidPhoneNumber(numeroTelefono)) {
+        if (numeroTelefono != null && !isValidPhoneNumber(numeroTelefono)) {
             throw new IllegalArgumentException("Número de teléfono inválido");
         }
         this.numeroTelefono = numeroTelefono;
     }
 
     /**
-     * Obtiene las notas sobre el cliente.
+     * Obtiene las notas adicionales sobre el cliente.
      *
-     * @return las notas sobre el cliente.
+     * @return las notas adicionales sobre el cliente.
      */
     public String getNotas() {
         return notas;
     }
 
     /**
-     * Establece las notas sobre el cliente.
+     * Establece las notas adicionales sobre el cliente.
      *
      * @param notas las nuevas notas sobre el cliente.
      */
@@ -183,21 +204,31 @@ public class Cliente {
         this.notas = notas;
     }
 
+    /**
+     * Retorna una representación en forma de cadena de la instancia actual del cliente.
+     *
+     * @return una cadena que representa al cliente.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Cliente {")
           .append("ID=").append(id)
           .append(", Nombre='").append(nombre).append('\'')
-          .append(", Nombre en Facebook='").append(nombreEnFacebook).append('\'')
+          .append(", Nombre en Redes='").append(nombreEnRedes).append('\'')
           .append(", Dirección=").append(direccion)
-          .append(", Fecha de Primera Compra=").append(fechaDePrimeraCompra)
           .append(", Número de Teléfono='").append(numeroTelefono).append('\'')
           .append(", Notas='").append(notas).append('\'')
           .append('}');
         return sb.toString();
     }
 
+    /**
+     * Determina si este cliente es igual a otro objeto.
+     *
+     * @param o el objeto a comparar.
+     * @return true si los objetos son iguales, false en caso contrario.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -206,13 +237,18 @@ public class Cliente {
         return id == cliente.id;
     }
 
+    /**
+     * Retorna un valor hash para el cliente.
+     *
+     * @return el valor hash.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(id, nombre, numeroTelefono);
     }
 
     /**
-     * Valida el número de teléfono utilizando libphonenumber.
+     * Valida el número de teléfono utilizando la biblioteca libphonenumber.
      *
      * @param numeroTelefono el número de teléfono a validar.
      * @return true si el número de teléfono es válido, false en caso contrario.
@@ -224,5 +260,14 @@ public class Cliente {
         } catch (NumberParseException e) {
             return false;
         }
+    }
+
+    /**
+     * Crea y devuelve una instancia de cliente por defecto.
+     *
+     * @return una instancia de cliente por defecto.
+     */
+    public static Cliente crearClientePorDefecto() {
+        return new Cliente(true);
     }
 }
